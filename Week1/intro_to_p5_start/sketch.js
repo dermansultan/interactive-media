30ee47fa7c7292ee80a29b245f2537902edded1c
dermansultan <abdurrehmansultan4@gmail.com>
2021-01-18 11:05:47 -0500

    first commit

---
+++ Week1/intro_to_p5_start/sketch.js
@@ -0,0 +1,12 @@
+preload = () => {
+    //put in here anything you want to load.
+}
+
+setup = () => {
+    //set up the document for how you want it to start
+}
+
+
+draw = () => {
+    //What you want to draw on the screen
+}
